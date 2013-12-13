77452feb69bce3c162a53ede23a25bd16daa53c4
Bradley Barnes <bradley.barnes21@gmail.com>
2013-12-12 18:39:41 -0600

    And color... but no backface

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.resources import resource_find
 from kivy.graphics.transformation import Matrix
 from kivy.graphics.opengl import *
 from kivy.graphics import *
+import ctypes
 from kivy.core.image import Image as Image
 import mini_geometry
 
@@ -45,25 +46,42 @@ colormap = {
     "Windows":(0.5, 0.5, 0.5),
 }
 nocolor={"nope":(0,0,0)}
+def loadTexture(filename, texUnit):
+    # Activate the texture unit (starting at 0); need 1 per texture
+    glActiveTexture(GL_TEXTURE0 + texUnit)
+    #texture = GLuint()
+    glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D,0)
+    # Texture does not wrap
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    # How is the image sampled?
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    # Load the image using PIL
+    image = Image("mini-diffuse.png").texture.flip_vertical()
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1024, 1024, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+
 
 class Renderer(Widget):
     def __init__(self, **kwargs):
-		self.canvas = RenderContext(compute_normal_mat=True)
-		self.canvas.shader.source = resource_find('simple.glsl')
-		self.scene =  mini_geometry.MiniGeometry()
-		self._tloc=resource_find('mini-diffuse.png')
-
-		with self.canvas:
+        self.canvas = RenderContext(compute_normal_mat=True)
+        self.canvas.shader.source = resource_find('simple.glsl')
+        self.scene =  mini_geometry.MiniGeometry()
+        self._tloc=resource_find('mini-diffuse.png')
+        self.texture=Image("mini-diffuse.png").texture
+        with self.canvas:
 			#Color(1,0,1)
-			#BindTexture(source='mini-diffuse.png', index=1)
+			BindTexture(texture=self.texture, index=1)
 			self.cb = Callback(self.setup_gl_context)
 			PushMatrix()
 			self.setup_scene()
 			PopMatrix()
 			self.cb = Callback(self.reset_gl_context)
-		#self.canvas['texture1']=1
-		super(Renderer, self).__init__(**kwargs)
-		Clock.schedule_interval(self.update_glsl, 1 / 60.)
+        self.canvas['tex']=1
+        super(Renderer, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update_glsl, 1 / 60.)
 
     def setup_gl_context(self, *args):
         glEnable(GL_DEPTH_TEST)
@@ -77,8 +95,6 @@ class Renderer(Widget):
         asp = self.width / float(self.height)
         #proj = Matrix().view_clip(-asp, asp, -1, 1, 1, 100, 1)
         proj = Matrix().view_clip(0, self.width, 0, self.height, 1, 100, 0)
-        #self.canvas['modelview_mat']=Window.render_context['modelview_mat']
-        #self.canvas['projection_mat'] = Window.render_context['projection_mat']
         self.canvas['projection_mat']=proj
         self.canvas['diffuse_light'] = (1.0, 1.0, 0.8)
         self.canvas['ambient_light'] = (0.1, 0.1, 0.1)
@@ -89,7 +105,7 @@ class Renderer(Widget):
         PushMatrix()
         Translate(300, 340, -30)
         self.rot = Rotate(1, 1, 1, 0)
-        self.canvas['tex']=Image('mini-diffuse.png').texture.flip_vertical()
+
         vertex_format=[
             ('v_pos', 3, 'float'),
             ('v_normal', 3, 'float'),
@@ -103,11 +119,11 @@ class Renderer(Widget):
             fmt=vertex_format,
             mode='triangles',
         )
-        BindTexture
-        self.mesh.texture=Image('mini-diffuse.png').texture.flip_vertical()
+
+        #self.mesh.texture=Image('mini-diffuse.png').texture.flip_vertical()
         PopMatrix()
 
-
+    
 class RendererApp(App):
     def build(self):
         return Renderer()
